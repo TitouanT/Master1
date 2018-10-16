eb80f580d397f7bd953a852739333fa32ec00794
TitouanT <titouan.teyssier@gmail.com>
2018-10-16 15:34:35 +0200

    fin de séance

--- reprCns/TP/TP1/2_circuit.pl
+++ reprCns/TP/TP1/2_circuit.pl
@@ -1,9 +1,18 @@
 comp_not(1, 0).
 comp_not(0, 1).
 
-comp_xor(X, X, 0) :- !.
-comp_xor(_, _, 1).
+comp_xor(0, 0, 0).
+comp_xor(0, 1, 1).
+comp_xor(1, 0, 1).
+comp_xor(1, 1, 0).
 
-comp_nand(1, 1, 0) :- !.
-comp_nand(_, _, 1).
+comp_nand(0, 0, 1).
+comp_nand(0, 1, 1).
+comp_nand(1, 0, 1).
+comp_nand(1, 1, 0).
 
+circuit(X, Y, Z) :-
+	comp_not(X, NOTX),
+	comp_nand(X, Y, NANDXY),
+	comp_xor(NANDXY, NOTX, NOTZ),
+	comp_not(NOTZ, Z).
